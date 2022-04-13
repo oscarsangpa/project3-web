bbdc0c1c036e49f2bff5391119b5f8124ee0fc9a
Oscar Sangrador <oscarsangrador@gmail.com>
2022-04-13 13:16:46 +0200

    try register. not working

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -2,6 +2,15 @@ const Login = () => {
   return ( 
     <>
       <h4>Login</h4>
+      <div>
+      <label>Email</label>
+      <input/>
+      <br/>
+
+      <label>Password</label>
+      <input/>
+      
+      </div>
     </>
    );
 }
--- src/views/Register/Register.jsx
+++ src/views/Register/Register.jsx
@@ -1,7 +1,63 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+// import { yupResolver } from '@hookform/resolvers/yup';
+// import * as yup from "yup";
+import { useNavigate } from 'react-router-dom';
+import InputGroup from "../../components/InputGroup/InputGroup";
+import { register as registerRequest } from '../../services/UsersService'
+
+// const schema = yup.object({
+//   email: yup.string().email().required(),
+//   name: yup.string().required(),
+//   password: yup.string().min(8, 'funciona').required()
+// }).required();
+
 const Register = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [backErrors, setBackErrors] = useState[{}]
+  const navigate = useNavigate()
+  // const { register, handleSubmit, formState:{ errors } } = useForm({
+  //   resolver: yupResolver(schema)
+  // });
+
+  const onSubmit = data => {
+    setBackErrors({})
+    setIsSubmitting(true)
+
+    registerRequest(data)
+      .then((user) => {
+        navigate('/login')
+      })
+      .catch(err => {
+        setBackErrors(err?.response?.data?.errors)
+      })
+      .finally(() => {
+        setIsSubmitting(false)
+      })
+  }
   return ( 
     <>
       <h4>Register</h4>
+      {/* <form onSubmit={handleSubmit(onSubmit)}>
+      <InputGroup 
+        label="Name"
+        id="name"
+        register={register}
+        type="name"
+      />
+      <InputGroup 
+        label="Email"
+        id="email"
+        register={register}
+        type="email"
+      />
+      <InputGroup 
+        label="Password"
+        id="password"
+        register={register}
+        type="password"
+      />
+      </form> */}
     </>
    );
 }
