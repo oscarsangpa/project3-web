370fe9de394d30a3798809f98b5e20ccde93d64b
Oscar Sangrador <oscarsangrador@gmail.com>
2022-04-16 11:50:00 +0200

    feat:navbar test

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,30 @@
+import { Link } from "react-router-dom";
+import './Navbar.css';
+
+
 const Navbar = () => {
   return ( 
     <>
-      <h4>Navbar</h4>
+      <nav className="Navbar">
+        <ul className="item">
+          <li>Who Is</li>
+          <li>
+            <Link to="/login">
+            <button>
+              Login
+            </button>
+            </Link>
+          </li>
+          <li>
+            <Link to="/register">
+            <button>
+              Register
+            </button>
+            </Link>
+          </li>
+        </ul>
+
+      </nav>
     </>
    );
 }
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,8 +1,10 @@
+import Navbar from "../../components/Navbar/Navbar";
 import SearchBar from "../../components/SearchBar/SearchBar";
 
 const Home = () => {
   return ( 
     <>
+      <Navbar/>
       <h3>HOME</h3>
       <SearchBar />
     </>
