55372a9c39aab5184cf6f483569128a0fb098255
Oscar Sangrador <oscarsangrador@gmail.com>
2022-04-23 11:13:39 +0200

    resolve conflicts with search views

--- src/router/MainRouter.js
+++ src/router/MainRouter.js
@@ -2,21 +2,21 @@ import * as React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import { ROUTE } from "./routes";
 import ProtectedRoute from "./ProtectedRoute";
-import Home from "../views/Home/Home";
-import Search from "../views/search/Search";
+import Home from "../views/Home/Home.jsx";
+// import Search from "../views/Search/Search.jsx";
 import Login from "../views/Login/Login.jsx";
 import Register from "../views/Register/Register";
 import Profile from "../views/Profile/Profile.jsx";
-import ActorDetail from "../views/detail/actorDetail/ActorDetail";
-import MovieDetail from "../views/detail/movieDetail/MovieDetail";
-import SerieDetail from "../views/detail/serieDetail/SerieDetail";
+import ActorDetail from "../views/detail/ActorDetail/ActorDetail"; 
+import MovieDetail from "../views/detail/MovieDetail/MovieDetail";
+import SerieDetail from "../views/detail/SerieDetail/SerieDetail";
 
 export default function MainRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={ROUTE.HOME} element={<Home />} />
-        <Route path={ROUTE.SEARCH} element={<Search />} />
+        {/* <Route path={ROUTE.SEARCH} element={<Search />} /> */}
         <Route path={ROUTE.LOGIN} element={<Login />} />
         <Route path={ROUTE.REGISTER} element={<Register />} />
         <Route
