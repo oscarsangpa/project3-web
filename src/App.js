fa3759128433ec546cad188d95c6ec627c10181c
Oscar Sangrador <oscarsangrador@gmail.com>
2022-05-07 13:32:41 +0200

    fix: rename footer

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { ThemeContextProvider } from "./contexts/ThemeContext";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
-import FooterComponent from "./components/Footer/Footer";
+import FooterComponent from "./components/FooterComponent/FooterComponent";
 // import "./style/styles.scss"
 
 const queryClient = new QueryClient();
